fb2bdd8b6298d81c75db1a7782546570aab2892a
Azrael <j.shepard.ramsay@gmail.com>
2015-09-18 10:59:40 -0400

    first commit

---
+++ Assets/WebcamTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebcamTest : MonoBehaviour {
+    WebCamTexture webcam;
+	// Use this for initialization
+	void Start () {
+       webcam = new WebCamTexture();
+       GetComponent<Renderer>().material.mainTexture = webcam;
+       webcam.Play();
+        //webcam.videoVerticallyMirrored
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(webcam.videoRotationAngle);
+	}
+}
